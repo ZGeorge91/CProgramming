9d4147d2f8c3004358ceaa36b16ed7cada37ad35
Zachary George <zgeorge3574@floridapoly.edu>
2025-08-22 14:35:59 +0000

    Created the frst program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-line comment
+08/20/2025
+Zach George
+The First Program in C.
+
+*/
+
+// One line comment
+
+// Preprocessor directive
+// stdio.h - stand input output
+// .h -header file
+#include <stdio.h>
+
+// Create the main function
+int main(void) {
+    printf("Hello\nWorld\n"); // \n (new line) - escape sequence
+}
+    printf("i\nam\nready\nto\ncode!\n");
+
+ return 0;
+}
